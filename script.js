257066bbadaddd697824cb820d63d88c267aeaf2
Abdee Buhary <buhary.abdee@gmail.com>
2021-06-01 08:58:14 -0400

    Starter files with code

---
+++ script.js
@@ -0,0 +1,17 @@
+// let choice=['Rock', 'Paper' , 'Scissor'];
+// let selector = Math.floor(Math.random() * choice.length);
+
+// computerPlay = (selector, choice[selector]);
+// const computerSelection = computerPlay;
+
+
+let playerChoice = prompt();
+
+
+
+
+
+
+// console.log(computerSelection);
+
+console.log(playerChoice);
